cc0f9f7a9ef506cccd2ab9de9a0ad059a3ec4b64
zxmh <1728026380@qq.com>
2024-06-18 16:56:21 +0800

    +mid70ladar&hikcamera&kinectcamera driver

---
+++ src/hikrobot_camera/README.md
@@ -0,0 +1,22 @@
+# HIKROBOT-MVS-CAMERA-ROS
+The ros driver package of Hikvision Industrial Camera SDK. Support configuration parameters, the parameters have been optimized, and the photos have been transcoded to rgb format.
+Please install mvs, https://blog.csdn.net/weixin_41965898/article/details/116801491
+
+# Install
+```
+mkdir -p ~/ws_hikrobot_camera/src
+git clone https://github.com/luckyluckydadada/HIKROBOT-MVS-CAMERA-ROS.git ~/ws_hikrobot_camera/src/hikrobot_camera
+cd ~/ws_hikrobot_camera
+catkin_make
+```
+# launch run
+```
+source ./devel/setup.bash 
+roslaunch hikrobot_camera hikrobot_camera.launch
+```
+# launch run
+use rviz subscribe topic： /hikrobot_camera/rgb
+```
+source ./devel/setup.bash 
+roslaunch hikrobot_camera hikrobot_camera_rviz.launch
+```
---
+++ src/hikrobot_camera/src/hikrobot_camera.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include <vector>
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <camera_info_manager/camera_info_manager.h>
+#include "hikrobot_camera.hpp"
+
+// 剪裁掉照片和雷达没有重合的视角，去除多余像素可以使rosbag包变小
+#define FIT_LIDAR_CUT_IMAGE false
+#if FIT_LIDAR_CUT_IMAGE
+    #define FIT_min_x 420
+    #define FIT_min_y 70
+    #define FIT_max_x 2450
+    #define FIT_max_y 2000
+#endif 
+
+using namespace std;
+using namespace cv;
+ 
+int main(int argc, char **argv)
+{
+    //********** variables    **********/
+    cv::Mat src;
+    //string src = "",image_pub = "";
+    //********** rosnode init **********/
+    ros::init(argc, argv, "hikrobot_camera");
+    ros::NodeHandle hikrobot_camera;
+    camera::Camera MVS_cap(hikrobot_camera);
+    //********** rosnode init **********/
+    image_transport::ImageTransport main_cam_image(hikrobot_camera);
+    image_transport::CameraPublisher image_pub = main_cam_image.advertiseCamera("/hikrobot_camera/rgb", 1000);
+
+    sensor_msgs::Image image_msg;
+    sensor_msgs::CameraInfo camera_info_msg;
+    cv_bridge::CvImagePtr cv_ptr = boost::make_shared<cv_bridge::CvImage>();
+    cv_ptr->encoding = sensor_msgs::image_encodings::BGR8;  // 就是rgb格式 
+    
+    //********** 10 Hz        **********/
+    ros::Rate loop_rate(10);
+
+    while (ros::ok())
+    {
+
+        loop_rate.sleep();
+        ros::spinOnce();
+
+        MVS_cap.ReadImg(src);
+        if (src.empty())
+        {
+            continue;
+        }
+        // cv::resize(src, src, cv::Size(1080, 720));
+#if FIT_LIDAR_CUT_IMAGE
+        cv::Rect area(FIT_min_x,FIT_min_y,FIT_max_x-FIT_min_x,FIT_max_y-FIT_min_y); // cut区域：从左上角像素坐标x，y，宽，高
+        cv::Mat src_new = src(area);
+        cv_ptr->image = src_new;
+#else
+        cv_ptr->image = src;
+#endif
+        image_msg = *(cv_ptr->toImageMsg());
+        image_msg.header.stamp = ros::Time::now();  // ros发出的时间不是快门时间
+        image_msg.header.frame_id = "hikrobot_camera";
+
+        camera_info_msg.header.frame_id = image_msg.header.frame_id;
+	    camera_info_msg.header.stamp = image_msg.header.stamp;
+        image_pub.publish(image_msg, camera_info_msg);
+
+        //*******************************************************************************************************************/
+    }
+    return 0;
+}
